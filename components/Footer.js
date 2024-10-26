6c51f720e0ef0cef9d6394f904def937a386040e
dulovskiy-vladislav <124919164+dulovskiy-vladislav@users.noreply.github.com>
2024-10-26 19:30:09 +0300

    Delete components directory

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__text">&copy; {new Date().getFullYear()} Митрофанова Софья</p>
-    </footer>
-  );
-}
-export default Footer;
